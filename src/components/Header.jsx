12cfabf72356016ef837fa4c682cfe34948f4e9c
Saurav Kumar <magnus@Sauravs-MacBook-Pro.local>
2025-05-06 16:22:49 +0530

    more changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import './Header.css'
-import logo from '../assets/Bite.png'
+import React, { useState } from 'react';
+import './Header.css';
+import logoImg from '../assets/Bite.png'; // Replace with your logo path
+import { FaBars } from 'react-icons/fa';
 
 const Header = () => {
-    return (
-        <>
-            <div className='firstline'>
-                <img src={logo} alt="logo" className='logo' />
-                <ul className='nav'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-        </>
-    )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Header
+  return (
+    <div className="firstline">
+      <img src={logoImg} alt="Logo" className="logo" />
+      <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+        <FaBars />
+      </button>
+      <ul className={`nav ${menuOpen ? 'show' : ''}`}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
